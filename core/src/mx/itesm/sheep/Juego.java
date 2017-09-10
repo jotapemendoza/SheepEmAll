b3a53dae02be5c04800849a6a5c49e7117961d25
josepablo <rmpros2@gmail.com>
2017-09-09 20:01:52 -0500

    Initial commit

---
+++ core/src/mx/itesm/sheep/Juego.java
@@ -0,0 +1,12 @@
+package mx.itesm.sheep;
+
+import com.badlogic.gdx.Game;
+
+public class Juego extends Game
+{
+	@Override
+	public void create() {
+		setScreen(new PantallaCargando(this));  // Splash Screen
+	}
+}
+
