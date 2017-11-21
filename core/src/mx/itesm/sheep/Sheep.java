fe7d306d7262179afbf36e9145a8332913793db6
Andreu <andree123_@hotmail.com>
2017-11-21 17:55:20 -0600

    rainbow sheep added fixed

--- core/src/mx/itesm/sheep/Sheep.java
+++ core/src/mx/itesm/sheep/Sheep.java
@@ -72,7 +72,7 @@ public class Sheep {
                     ancho = 108;
                     alto = 140;
                     frames = region.split(ancho,alto);
-                    animacion = new Animation(0.20f, frames[0][0], frames[0][1]);
+                    animacion = new Animation(0.20f, frames[0][0], frames[0][1], frames[0][2], frames[0][3]);
                     animacion.setPlayMode(Animation.PlayMode.LOOP);
 
                     anchoM = 114;
